a7c8c7b944e4e0dcbb6d8e20e6121f3e703738d4
malaikashinchan <malaimihi1405@gmail.com>
2026-04-15 11:33:39 +0530

    RBAC ENABLED FOR KEY VAULTS

--- terraform/azure_core.tf
+++ terraform/azure_core.tf
@@ -94,7 +94,7 @@ resource "azurerm_key_vault" "ecom" {
   sku_name                   = "standard"
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
-
+  enable_rbac_authorization = true
   tags = local.tags
 }
 
